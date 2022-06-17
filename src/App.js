3fbffa74f453754d5875add1518ce3148adf641e
IrumiDesu7 <ikalam89@gmail.com>
2022-06-18 00:57:01 +0800

    add styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+  function reset() {
+    setCount(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='counterContainer'>
+      <div className='counter'>{count}</div>
+      <div className='buttonContainer'>
+        <button onClick={decrease} className='decrease'>
+          -
+        </button>
+        <button onClick={reset} className='reset'>
+          Reset
+        </button>
+        <button onClick={increase} className='increase'>
+          +
+        </button>
+      </div>
     </div>
   );
 }
